185a918be5f8e54c9d3b645d6fa7a7aacfaca30a
Kevin <kevbesset@gmail.com>
2024-05-19 10:20:34 +0200

    add live trans route

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import Demo from "./pages/Demo";
 import Error from "./pages/Error";
 import Pals from "./pages/Pals";
+import LiveTranslation from "./pages/Pals/LiveTranslation";
 import Scenario from "./pages/Scenario";
 import Setting from "./pages/Setting";
 import Store from "./pages/Store";
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
       {
         path: "pals",
         element: <Pals />,
+      },
+      {
+        path: "pals/live-translation",
+        element: <LiveTranslation />,
       },
     ],
   },
