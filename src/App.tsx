4440f683a7126b21a2577f563c6be5da15d46184
Kevin <kevbesset@gmail.com>
2024-05-23 09:11:08 +0200

    clean app dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styles from "./App.module.scss";
-import Toolbar from "./components/layouts/Toolbar";
 
 const { block } = bem(styles);
 
 export default function App() {
   return (
     <div className={block()}>
-      <Toolbar />
+      {/* <Toolbar /> */}
       <Outlet />
       <ToastContainer />
     </div>
